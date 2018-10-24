b3ee8da6616949c3dc6eab77d9c42f75507b2771
Matteo Bonora <bonora.matteo@gmail.com>
2018-10-24 10:51:49 +0200

    Add hw homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Hello world')
+const PORT = 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
