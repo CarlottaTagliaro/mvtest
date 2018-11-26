2b49429da8bb60f37dc4fac12db93974b0525876
Hook25 <massigi@live.it>
2018-11-26 14:21:12 +0100

    Test case constructor type inference

---
+++ tests/class_v.test.js
@@ -0,0 +1,16 @@
+class_v = require('../controllers/class_view.js');
+
+test('as_defined_in_spec', ()=>{
+  let spec = ['getAll', 'getById', 'create', 'edit', 'delete'];
+  let cv = new class_v(null);
+  for(method of spec){
+    expect(cv[method]).toBeDefined();
+  }
+});
+
+test('constructor_type_inference', ()=>{
+  let not_working_values = [true, 0, "", undefined, null];
+  for(val of not_working_values){
+    expect(()=>{ new class_v(val); }).toThrow();
+  }  
+});
