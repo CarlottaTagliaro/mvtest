4640631c87e397c75774beb7b70972d1ff33ce84
Hook25 <massigi@live.it>
2018-12-04 20:49:43 +0100

    Finally tested actual functionality

--- tests/class_v.test.js
+++ tests/class_v.test.js
@@ -1,4 +1,17 @@
-class_v = require('../controllers/db/class_view.js');
+let class_v = require('../controllers/db/class_view.js');
+const db = require('../controllers/db');
+
+const classes_v = [{
+	id: expect.any(Number),
+	name: expect.any(String)
+}];
+
+const class_template = {
+	id : expect.any(Number),
+	name : expect.any(String)
+}
+
+let gc_class = -1;
 
 test('as_defined_in_spec', ()=>{
   let spec = ['getAll', 'getById', 'create', 'edit', 'delete'];
@@ -13,4 +26,39 @@ test('constructor_type_inference', ()=>{
   for(val of not_working_values){
     expect(()=>{ new class_v(val); }).toThrow();
   }  
-});  
+});
+
+test('GET /api/class - get all classes', () => {
+	return expect(db.class_v.getAll()).resolves.toEqual(expect.arrayContaining(classes_v));
+});
+
+test('POST /api/class - creates a class and returns its id', () => {
+	let class_tmp = {
+			id: 200,
+			name: "name"
+		},
+		expected = {
+			id: expect.any(Number)
+		};
+
+	//expect.assertions(1);
+	return db.class_v.create(class_tmp).then((res) => {
+		console.log('CREATE RETURNS', res)
+		gc_class = res.id;
+		expect(res).toMatchObject(expected);
+	});
+});
+
+test('GET /api/class/:id - get class by id', () => {
+	return expect(db.class_v.getById(gc_class)).resolves.toMatchObject(class_template);
+});
+
+test('DELETE /api/class/:id - deletes class', () => {
+	console.warn(">>>>>>>>>>" + gc_class);
+	return expect(db.class_v.delete(gc_class)).resolves.toBeUndefined();
+});
+
+afterAll(() => {
+	console.warn("Closing DB");
+	db.close();
+});
