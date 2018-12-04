8594f7d10eb2fd615ffd8fb60b2cf0d2a0da776e
Carlotta Tagliaro <carlotta.tagliaro@gmail.com>
2018-12-04 16:32:24 +0100

    split exams from index.js

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -5,59 +5,7 @@ const db = require('../../controllers/db');
 router.use('/auth', require('./auth.js'));
 router.use('/users', require('./users.js'));
 router.use('/tasks', require('./tasks.js'));
-
-router.get('/exams', (req, res) => {
-  db.exam.getAll().then((data) => {
-    res.status(200);
-    res.json(data);
-  }).catch((err) => {
-    res.status(500);
-    res.send(err.message);
-  });
-});
-
-router.get('/exams/:id', (req, res) => {
-  db.exam.getOne(parseInt(req.params.id)).then((data) => {
-    res.status(200);
-    res.json(data);
-  }).catch((err) => {
-    switch (err.errno) {
-      case 404:
-        res.status(404);
-        res.send(err.message);
-        break;
-    }
-    res.status(500);
-    res.send(err.message);
-  });
-});
-
-router.post('/exams', (req, res) => {
-
-  let exam = {
-    id_creator: req.body.id_creator,
-    tasks: req.body.tasks
-  };
-
-  db.exam.create(exam).then(data => {
-    res.status(201);
-    res.send(data);
-  }).catch(err => {
-    res.status(500);
-    res.send(err);
-  });
-});
-
-
-router.delete('/exams/:id', (req, res) => {
-  db.exam.delete(parseInt(req.params.id)).then(data => {
-    res.status(204);
-    res.send();
-  }).catch(err => {
-    res.status(500);
-    res.send(err.message);
-  });
-});
+router.use('/exams', require('./exams.js'));
 
 router.get('/assignments', (req, res) => {
   db.assignment.getAll().then((data) => {
