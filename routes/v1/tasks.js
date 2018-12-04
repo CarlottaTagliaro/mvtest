8168fcc2948bb9697d52a95b735a260b4922ee59
Matteo Bonora <bonora.matteo@gmail.com>
2018-12-04 21:49:21 +0100

    Fix task functions

--- routes/v1/tasks.js
+++ routes/v1/tasks.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
 		res.status(200);
 		res.json(data);
 	}).catch((err) => {
-		res.status(500);
+		res.status(err.code || 500);
 		res.send(err.message);
 	});
 });
@@ -18,7 +18,7 @@ router.get('/:id', (req, res) => {
 		res.status(200);
 		res.json(data);
 	}).catch((err) => {
-		res.status(err.errno || 500);
+		res.status(err.code || 500);
 		res.send(err.message);
 	});
 });
@@ -28,7 +28,7 @@ router.put('/:id', (req, res) => {
 		res.status(204);
 		res.send();
 	}).catch((err) => {
-		res.status(err.errno || 500);
+		res.status(err.code || 500);
 		res.send(err.message);
 	});
 });
@@ -38,39 +38,18 @@ router.delete('/:id', (req, res) => {
 		res.status(204);
 		res.send();
 	}).catch((err) => {
-		res.status(err.errno || 500);
+		res.status(err.code || 500);
 		res.send(err.message);
 	});
 });
 
 router.post('/', (req, res) => {
-
-	let text = {
-		question: req.body.question
-	};
-	let task = {
-		points: req.body.points,
-		id_type: 1
-	}
-
-	if (req.body.answer1 || req.body.answer2 || req.body.answer3 || req.body.answer4) {
-		text.choices = [req.body.answer1, req.body.answer2, req.body.answer3, req.body.answer4];
-
-		if (req.body.multipleChoice == 'on')
-			task.id_type = 3;
-		else
-			task.id_type = 2;
-	}
-	task.text = text
-
-	console.log(task)
-
-	db.task.create(task).then(data => {
+	db.task.create(req.body).then(data => {
 		res.status(201);
 		res.send(data);
 	}).catch(err => {
-		res.status(500);
-		res.send(err);
+		res.status(err.code || 500);
+		res.send(err.message);
 	});
 });
 
@@ -79,7 +58,7 @@ router.delete('/:id', (req, res) => {
 		res.status(204);
 		res.send();
 	}).catch(err => {
-		res.status(500);
+		res.status(err.code || 500);
 		res.send(err.message);
 	});
 });
